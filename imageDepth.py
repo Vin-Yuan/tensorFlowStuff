ff6684ffd0f0fdf2dc788912f6e60cb65e7db9a1
Vin-Yuan <vin-yuan@hotmail.com>
2016-06-02 18:03:17 +0800

    add sth stuff on inference

--- imageDepth.py
+++ imageDepth.py
@@ -1,4 +1,8 @@
+import tensorflow as tf
 def inference(images):
 #the image is [batch_size, IMAGE_SIZE, IMAGE_SIZE, 3]
 #return :
 # Logits.
+
+with tf.variable_scope('conv1') as scope:
+  kernel = variable_with_weight_decay(
